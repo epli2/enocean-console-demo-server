cefc9528514a11f0672e306e2ca4021bcf863584
epli2 <motoryo1@gmail.com>
2018-01-10 12:21:18 +0900

    mongodbに保存された値を取得するAPIを追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from pymongo import MongoClient
 
 import mqtt
 import audio
@@ -12,11 +13,30 @@ socketio = SocketIO(app)
 def index():
     return render_template('index.html')
 
+@app.route('/api/history/<paramname>/<int:length>', methods=['GET'])
+def history(paramname, length):
+    if paramname == 'temperature':
+        topic = 'sensor/04016897/Temperature'
+    elif paramname == 'humidity':
+        topic = 'sensor/04016897/Humidity'
+    elif paramname == 'illumination':
+        topic = 'sensor/04016777/Illumination'
+    else:
+        return '{ }'
+
+    data = []
+    for i in co.find({'topic': topic}).sort('timestamp', -1).limit(length):
+        data.append('{"value":%s,"timestamp":"%s","ret":"%s"}' % (i['value'], i['timestamp'], i['ret']))
+    return '{"data":[%s]}' % ','.join(data)
+
 @socketio.on('connect', namespace='/api/socket')
 def test_connect():
     print('connected from client')
 
 if __name__ == '__main__':
+    client = MongoClient('localhost', 27017)
+    db = client.sensordb
+    co = db.sensordata
     mqtt.start(socketio)
     audio.start(socketio)
     socketio.run(app, host='0.0.0.0', debug=True)
